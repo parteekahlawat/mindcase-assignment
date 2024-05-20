0b5e54c3836584c49cc91604e149c7e8f1030222
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2024-05-20 20:26:07 +0530

    Update login.tsx

--- src/app/(comps)/login/login.tsx
+++ src/app/(comps)/login/login.tsx
@@ -5,7 +5,7 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useRouter } from 'next/navigation'
 import { useEffect } from "react";
 
-const supabase = createClient("https://aqbjpffvdeazeyfgcame.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxYmpwZmZ2ZGVhemV5ZmdjYW1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1NTQxODQsImV4cCI6MjAyOTEzMDE4NH0.0Rg35j8XoXQuKdyyWnA4zS1UbYutfiI8q6QMWKp-pSA")
+const supabase = createClient("#Link", "#ID")
 export default function Login() {
     const router = useRouter();
 
